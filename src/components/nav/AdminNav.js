a8eb22d554b57d0fb5eb1613a18ca7680ac3a2b5
Nathan Malone <nathanamalone@gmail.com>
2022-09-29 16:10:05 -0500

    completed capstone

--- src/components/nav/AdminNav.js
+++ src/components/nav/AdminNav.js
@@ -49,41 +49,4 @@ export const AdminNav = () => {
             }
           </Navbar>
         );
-      
-
-    return (
-        <ul className="navbar">
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/myBooks">My Books</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/allBooks">All Books</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/addBook">Add Book</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/members">Members</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/newMember">New Member</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/bazaarContainer">Bayport Bazaar</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/bookStatus">Book Status</Link>
-            </li>
-            {
-                localStorage.getItem("book_user")
-                    ? <li className="navbar__item navbar__logout">
-                        <Link className="navbar__link" to="" onClick={() => {
-                            localStorage.removeItem("book_user")
-                            navigate("/", {replace: true})
-                        }}>Logout</Link>
-                    </li>
-                    : ""
-            }
-        </ul>
-    )
 }
